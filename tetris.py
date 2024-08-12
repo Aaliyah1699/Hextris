3416b409b4a1d9a03627923af3991e02099f6ceb
Aaliyah <93296412+Aaliyah1699@users.noreply.github.com>
2024-08-12 15:53:12 -0500

    Create grid function

--- tetris.py
+++ tetris.py
@@ -6,12 +6,24 @@ screen = pygame.display.set_mode((500, 600))
 pygame.display.set_caption('Hextris')
 game_over = False
 
+
+# Grid
+def draw_grid(rows, cols, grid_size):
+    # Create grid and place on screen
+    for y in range(rows):
+        for x in range(cols):
+            pygame.draw.rect(screen, (100, 100, 100), [x * grid_size, y * grid_size, grid_size, grid_size], 1)
+
+
 while not game_over:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game_over = True
 
     screen.fill((0, 0, 0))
+    # Grid
+    draw_grid(10, 10, 30)
+
     pygame.display.update()
 
 pygame.quit()
