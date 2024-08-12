f8eef9869357fcc7187702570ae511b7ca5d7798
Aaliyah <93296412+Aaliyah1699@users.noreply.github.com>
2024-08-12 16:47:37 -0500

    Create blocks shapes

--- tetris.py
+++ tetris.py
@@ -6,6 +6,17 @@ screen = pygame.display.set_mode((600, 600))  # Screen size
 pygame.display.set_caption('Hextris')
 game_over = False
 
+# Blocks shape
+blocks = [
+    [[1, 4, 7], [3, 4, 5]],  # Vertical & Horizontal
+    [[1, 3, 4, 5, 7]],  # Cross
+    [[0, 1, 4, 5], [1, 3, 4, 6]],  # Z shape pt 1
+    [[1, 2, 3, 4], [0, 3, 4, 7]],  # Z shape pt2
+    [[0, 1, 3, 6], [0, 1, 2, 5], [2, 5, 7, 8], [3, 6, 7, 8]],  # L shape pt1
+    [[1, 2, 5, 8], [5, 6, 7, 8], [0, 3, 6, 7], [0, 1, 2, 3]],  # L shape pt2
+    [[4, 6, 7, 8], [0, 3, 4, 6], [0, 1, 2, 4], [2, 4, 5, 8]]  # T shape
+]
+
 
 # Grid
 def draw_grid():  # Passing in (cols, rows, grid_size, x_gap, y_gap)
