422636bde9af04335af32516615c087808984405
Aaliyah <93296412+Aaliyah1699@users.noreply.github.com>
2024-08-12 16:08:43 -0500

    Create and implement base grid

--- tetris.py
+++ tetris.py
@@ -2,18 +2,26 @@ import pygame
 
 pygame.init()
 
-screen = pygame.display.set_mode((500, 600))
+screen = pygame.display.set_mode((600, 600))  # Screen size
 pygame.display.set_caption('Hextris')
 game_over = False
 
 
 # Grid
-def draw_grid(rows, cols, grid_size):
+def draw_grid():  # Passing in (cols, rows, grid_size, x_gap, y_gap)
     # Create grid and place on screen
     for y in range(rows):
         for x in range(cols):
-            pygame.draw.rect(screen, (100, 100, 100), [x * grid_size, y * grid_size, grid_size, grid_size], 1)
+            pygame.draw.rect(screen, (100, 100, 100),
+                             [x * grid_size + x_gap, y * grid_size + y_gap, grid_size, grid_size], 1)
 
+
+# Have grid spread over screen size
+grid_size = 30
+cols = screen.get_width() // grid_size
+rows = screen.get_height() // grid_size
+x_gap = (screen.get_width() - cols * grid_size) // 2
+y_gap = (screen.get_height() - rows * grid_size) // 2
 
 while not game_over:
     for event in pygame.event.get():
@@ -22,7 +30,7 @@ while not game_over:
 
     screen.fill((0, 0, 0))
     # Grid
-    draw_grid(10, 10, 30)
+    draw_grid()  # Passing in (cols, rows, grid_size, x_gap, y_gap)
 
     pygame.display.update()
 
